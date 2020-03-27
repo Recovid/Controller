b3a1398965aeead4acccfd7cf492c468e79ad6e1
cfromentin <cyril.fromentin@gmail.com>
2020-03-27 16:36:17 +0100

    Add files via upload

---
+++ Arduino/ProgrammeInit/mbed/scheduling.h
@@ -0,0 +1,45 @@
+/*
+ * scheduling.h
+ *
+ *  Created on: 3 sept. 2019
+ *      Author: Cyril
+ */
+
+#ifndef OSAL_THREADING_SCHEDULING_H_
+#define OSAL_THREADING_SCHEDULING_H_
+#include <Arduino.h>
+#include <Thread.h>
+#include <mbed.h>
+
+using namespace rtos;
+
+
+class scheduling {
+public:
+
+	/* Default Constructor */
+	scheduling();
+
+	/* Deconstructor */
+	virtual ~scheduling();
+	static void schedulerSysTick();
+	static void scheduler5ms();
+	static void scheduler1ms();
+
+private:
+	// stop copy constructor and = being called erroneously
+	// do not implement
+	scheduling(const scheduling &other);
+	scheduling& operator=(const scheduling&);
+    static int cnt;
+
+
+#ifdef UNIT_TEST
+    friend class scheduler_test;
+#endif
+#ifdef UNIT_TEST
+    friend class scheduling_test;
+#endif
+};
+
+#endif /* OSAL_THREADING_SCHEDULING_H_ */
