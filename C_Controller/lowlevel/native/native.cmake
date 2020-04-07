698eed60a471bbbc3d663a72271d9e12aa4977a1
Siméon Marijon <simeon.marijon@minmaxmedical.com>
2020-04-07 17:32:17 +0200

    Add FreeRTOS-Simu

--- C_Controller/lowlevel/native/native.cmake
+++ C_Controller/lowlevel/native/native.cmake
@@ -8,5 +8,20 @@ set(CMAKE_OBJCOPY objcopy)
 set(CMAKE_OBJDUMP objdump)
 set(SIZE size)
 
+SET(FREERTOS_DIR_X86 ${LOWLEVEL_SRC_DIR}/FreeRTOS-Sim/)
+SET(FREERTOS_SRC_DIR_X86 ${FREERTOS_DIR_X86}/Source/)
+SET(FREERTOS_INC_DIR_X86 ${FREERTOS_DIR_X86}/Source/include/)
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pthread") 
+set(FREERTOS_SRC_X86 
+	${LOWLEVEL_SRC_DIR}/vassertcall.c
+	${FREERTOS_SRC_DIR_X86}/croutine.c
+	${FREERTOS_SRC_DIR_X86}/event_groups.c
+	${FREERTOS_SRC_DIR_X86}/list.c
+	${FREERTOS_SRC_DIR_X86}/queue.c
+	${FREERTOS_SRC_DIR_X86}/tasks.c
+	${FREERTOS_SRC_DIR_X86}/timers.c
+	${FREERTOS_SRC_DIR_X86}/portable/MemMang/heap_3.c
+	${FREERTOS_SRC_DIR_X86}/portable/GCC/POSIX/port.c)
 
-
+include_directories(${LOWLEVEL_SRC_DIR} ${FREERTOS_INC_DIR_X86} ${FREERTOS_SRC_DIR_X86} ${FREERTOS_SRC_DIR_X86}/portable/GCC/POSIX/)
+target_sources(${EXECUTABLE} PRIVATE ${SOURCES} ${FREERTOS_SRC_X86})
