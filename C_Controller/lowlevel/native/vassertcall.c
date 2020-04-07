ccc939971ed0f633342307525e0aee5dd5c8ed2e
Siméon Marijon <simeon.marijon@minmaxmedical.com>
2020-04-07 17:32:17 +0200

    Add FreeRTOS-Simu

---
+++ C_Controller/lowlevel/native/vassertcall.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+void vAssertCalled( unsigned long ulLine, const char * const pcFileName )
+{
+ 	taskENTER_CRITICAL();
+	{
+        printf("[ASSERT] %s:%lu\n", pcFileName, ulLine);
+        fflush(stdout);
+	}
+	taskEXIT_CRITICAL();
+	exit(-1);
+}
