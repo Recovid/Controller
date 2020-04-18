66194629b67a75b69d879d70ccdef4021ef0a505
arnaud.clere <arnaud.clere@minmaxmedical.com>
2020-04-18 17:37:02 +0200

    cosmetic change

--- C_Controller/sensing.h
+++ C_Controller/sensing.h
@@ -9,12 +9,9 @@ float get_sensed_VolM_Lpm();
 float get_sensed_VTi_mL  ();
 float get_sensed_VTe_mL  ();
 
-//! \returns max sensed Paw during Insufflation
-float get_sensed_Pcrete_cmH2O();
-//! \returns sensed Paw at end of Plateau
-float get_sensed_Pplat_cmH2O ();
-//! \returns sensed Paw at end of Exhale
-float get_sensed_PEP_cmH2O   ();
+float get_sensed_Pcrete_cmH2O(); //!< \returns max sensed Paw during Insufflation
+float get_sensed_Pplat_cmH2O (); //!< \returns sensed Paw at end of Plateau
+float get_sensed_PEP_cmH2O   (); //!< \returns sensed Paw at end of Exhale
 
 float get_sensed_VMe_Lpm     ();
 
