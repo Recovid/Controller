123b155a456809e54c823850d8b5774ba73df709
Siméon Marijon <simeon.marijon@minmaxmedical.com>
2020-04-19 13:48:35 +0200

    lowlevel: lights/buzzer: fix on/off feature

--- C_Controller/lowlevel/stm32f303/lights.c
+++ C_Controller/lowlevel/stm32f303/lights.c
@@ -4,22 +4,22 @@
 
 
 bool light_nucleo(OnOff val) {
-	HAL_GPIO_WritePin(NUCLEO_LED_GPIO_Port, NUCLEO_LED_Pin, val?GPIO_PIN_SET:GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(NUCLEO_LED_GPIO_Port, NUCLEO_LED_Pin, val == On ?GPIO_PIN_RESET:GPIO_PIN_SET);
   return true;
 }
 
 
 bool light_yellow(OnOff val) {
-	HAL_GPIO_WritePin(MAT_LED_ORANGE_GPIO_Port, MAT_LED_ORANGE_Pin, val?GPIO_PIN_SET:GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(MAT_LED_ORANGE_GPIO_Port, MAT_LED_ORANGE_Pin, val == On ?GPIO_PIN_SET:GPIO_PIN_RESET);
   return true;
 }
 
 bool light_red   (OnOff val) {
-	HAL_GPIO_WritePin(MAT_LED_RED_GPIO_Port, MAT_LED_RED_Pin, val?GPIO_PIN_SET:GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(MAT_LED_RED_GPIO_Port, MAT_LED_RED_Pin, val == On ?GPIO_PIN_SET:GPIO_PIN_RESET);
   return true;
 }
 
 bool light_green (OnOff val) {
-	HAL_GPIO_WritePin(MAT_LED_GREEN_GPIO_Port, MAT_LED_GREEN_Pin, val?GPIO_PIN_SET:GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(MAT_LED_GREEN_GPIO_Port, MAT_LED_GREEN_Pin, val == On ?GPIO_PIN_SET:GPIO_PIN_RESET);
   return true;
 }
