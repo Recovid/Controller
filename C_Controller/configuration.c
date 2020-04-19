96720279706fc7ca8a45d848a72e57171fc26827
Siméon Marijon <simeon.marijon@minmaxmedical.com>
2020-04-19 10:22:04 +0200

    configuration.h : Fix compilation (do not define a symbole in header)

--- C_Controller/configuration.c
+++ C_Controller/configuration.c
@@ -2,6 +2,16 @@
 
 #include "lowlevel/include/lowlevel.h"
 
+
+
+const float P_PLATEAU_SLOPE = 0.1f; // V1 was 0.05
+const float P_PLATEAU_MEAN  = 0.2f; // V1 was 0.1
+
+const float MOTOR_STEP_TIME_US_MIN = 110.f;
+
+const float CALIB_STEP_TIME_S = 1.f/*s*/ / MOTOR_STEPS_MAX; // V1 was 1/(MOTOR_STEPS_MAX*200/360) ?!
+const float CALIB_MAGIC_RATIO = 1.f; // V1 was 0.8
+
 // ------------------------------------------------------------------------------------------------
 //! Environment simulation
 
