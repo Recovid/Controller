ee80663fb6e4ee3b463219814c29102cfb8a24a5
Vivien Delmon <vivien.delmon@minmaxmedical.com>
2020-07-01 23:20:32 +0200

    Also add 3 digits to Paw

--- sources/src/protocol.c
+++ sources/src/protocol.c
@@ -52,7 +52,7 @@ void replace_int_with_padding(char* frame, int value, int size, int base)
     }
 }
 
-static const char DATA_pattern[] = "DATA msec_:...... Vol__:....... Deb__:....... Paw__:...." CS8 CS8_VALUE;
+static const char DATA_pattern[] = "DATA msec_:...... Vol__:....... Deb__:....... Paw__:......." CS8 CS8_VALUE;
 static char DATA_frame[sizeof(DATA_pattern)];
 
 bool send_DATA(float P_cmH2O, float VolM_Lpm, float Vol_mL)
@@ -71,14 +71,14 @@ bool send_DATA(float P_cmH2O, float VolM_Lpm, float Vol_mL)
     *strchr(DATA_frame, '.') = sign(VolM_Lpm);
     replace_int_with_padding(DATA_frame, roundf(VolM_Lpm * 1000), 6, 10);
     *strchr(DATA_frame, '.') = sign(P_cmH2O);
-    replace_int_with_padding(DATA_frame, roundf(P_cmH2O), 3, 10);
+    replace_int_with_padding(DATA_frame, roundf(P_cmH2O * 1000), 6, 10);
 
     replace_int_with_padding(DATA_frame, checksum8(DATA_frame), 2, 16);
 
     return send(DATA_frame);
 }
 
-static const char DATA_X_pattern[] = "DATA msec_:...... Vol__:....... Deb__:....... Paw__:.... PPLAT:.. PEP__:.." CS8 CS8_VALUE;
+static const char DATA_X_pattern[] = "DATA msec_:...... Vol__:....... Deb__:....... Paw__:....... PPLAT:.. PEP__:.." CS8 CS8_VALUE;
 static char DATA_X_frame[sizeof(DATA_X_pattern)];
 
 bool send_DATA_X(float P_cmH2O, float VolM_Lpm, float Vol_mL, float Pplat_cmH2O, float PEP_cmH2O)
@@ -98,7 +98,7 @@ bool send_DATA_X(float P_cmH2O, float VolM_Lpm, float Vol_mL, float Pplat_cmH2O,
     *strchr(DATA_X_frame, '.') = sign(VolM_Lpm);
     replace_int_with_padding(DATA_X_frame, roundf(VolM_Lpm * 1000)   , 6, 10);
     *strchr(DATA_X_frame, '.') = sign(P_cmH2O);
-    replace_int_with_padding(DATA_X_frame, roundf(P_cmH2O)    , 3, 10);
+    replace_int_with_padding(DATA_X_frame, roundf(P_cmH2O * 1000)    , 6, 10);
     replace_int_with_padding(DATA_X_frame, roundf(Pplat_cmH2O), 2, 10);
     replace_int_with_padding(DATA_X_frame, roundf(PEP_cmH2O)  , 2, 10);
 
